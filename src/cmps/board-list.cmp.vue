0859240d843d7e2f7ff04cc44902621a8279d9d6
capos <caposher@gmail.com>
2021-11-30 23:04:46 +0200

    add css to list

--- src/cmps/board-list.cmp.vue
+++ src/cmps/board-list.cmp.vue
@@ -1,45 +1,43 @@
 <template>
-  <li>
-    <header>
-      <h4>{{list.title}}</h4>
-      <button class="actions">...</button>
-    </header>
-    <ul v-for="card in list.cards" :key="card.id">
-    <!-- v-for cards in list.cards  :mini-list="mini-list"-->
-    <card-list :card="card" :list="list"></card-list>
-    </ul>
-    <footer>
-      <button @click="addCard">+Add card</button>
-    </footer>
-  </li>
+    <li class="list">
+        <header>
+            <h4>{{ list.title }}</h4>
+            <button class="actions">...</button>
+        </header>
+        <ul v-for="card in list.cards" :key="card.id">
+            <!-- v-for cards in list.cards  :mini-list="mini-list"-->
+            <card-list :card="card" :list="list"></card-list>
+        </ul>
+        <footer>
+            <button @click="addCard">+Add card</button>
+        </footer>
+    </li>
 </template>
 
 <script>
-import cardList from "./card-list.cmp.vue";
+import cardList from './card-list.cmp.vue';
 
 // props- list
 export default {
-  props:{
-    list:{
-      type:Object
-    }
-  },
-  methods:{
-    async addCard(){
-       const title = prompt('card title')
-      try{
-        this.$store.dispatch({type:'addCard',boardId:this.$store.getters.boardId,list: this.list, title})
-      }
-      catch(err){
-        console.log('cant add card', err);
-      }
-    }
-  },
-  components: {
-    cardList
-  }
+    props: {
+        list: {
+            type: Object,
+        },
+    },
+    methods: {
+        async addCard() {
+            const title = prompt('card title');
+            try {
+                this.$store.dispatch({ type: 'addCard', boardId: this.$store.getters.boardId, list: this.list, title });
+            } catch (err) {
+                console.log('cant add card', err);
+            }
+        },
+    },
+    components: {
+        cardList,
+    },
 };
 </script>
 
-<style>
-</style>
+<style></style>
--- src/pages/board.vue
+++ src/pages/board.vue
@@ -1,50 +1,64 @@
 <template>
-  <section>
-    <board-header />
-    <ul>
-      <li v-for="list in board.lists" :key="list.id">
-        <board-list :list="list"></board-list>
-      </li>
-      <li @click="addList">Add list</li>
-    </ul>
-  </section>
+    <section>
+        <board-header />
+        <ul class="board">
+            <li v-for="list in board.lists" :key="list.id" class="list-wrapper">
+                <board-list :list="list"></board-list>
+            </li>
+            <li @click="addList">Add list</li>
+        </ul>
+    </section>
 </template>
 
 <script>
-import boardHeader from "../cmps/board-header.cmp.vue";
-import mainMenu from "../cmps/main-menu.cmp.vue";
-import boardMenu from "../cmps/board-menu.cmp.vue";
-import boardList from "../cmps/board-list.cmp.vue";
+import boardHeader from '../cmps/board-header.cmp.vue';
+import mainMenu from '../cmps/main-menu.cmp.vue';
+import boardMenu from '../cmps/board-menu.cmp.vue';
+import boardList from '../cmps/board-list.cmp.vue';
 
 export default {
-  data() {
-    return {};
-  },
-  computed: {
-    board() {
-      console.log(this.$store.getters.board);
-      return this.$store.getters.board;
-    }
-  },
-  methods: {
-    async addList() {
-      const title = prompt('list title')
-      try{
-        this.$store.dispatch({type:'addList', title, boardId:this.board._id})
-      }
-      catch(err){
-        console.log('cant add list', err);
-      }
-    }
-  },
-  components: {
-    boardHeader,
-    boardMenu,
-    mainMenu,
-    boardList
-  }
+    data() {
+        return {
+            board: {
+                _id: 'oqoY7',
+                title: 'New Board',
+                createdAt: '',
+                createdBy: 'user',
+                style: null,
+                labels: ['label1', 'label2', 'label3'],
+                members: [],
+                lists: [
+                    { id: 'LO1GIK', title: 'sd', cards: [] },
+                    { id: 'LJeJBK', title: 'bxcv', cards: [] },
+                    { id: 'L1l0yD', title: 'jojo', cards: [] },
+                ],
+            },
+        };
+    },
+
+    // computed: {
+    //     board() {
+    //         console.log(this.$store.getters.board);
+    //         return this.$store.getters.board;
+    //     },
+    // },
+    methods: {
+        async addList() {
+            const title = prompt('list title');
+            try {
+                this.$store.dispatch({ type: 'addList', title, boardId: this.board._id });
+            } catch (err) {
+                console.log('cant add list', err);
+            }
+        },
+    },
+    components: {
+        boardHeader,
+        boardMenu,
+        mainMenu,
+        boardList,
+    },
 };
 </script>
 
-<style>
-</style>
+<style></style>

3bb272d2b940f29239aad05a727ac2632f892bdc
capos <caposher@gmail.com>
2021-12-01 10:35:51 +0200

    commit

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,54 +1,53 @@
 <template>
   <section class="home">
     <ul>
-      <li v-for="board in boards" :key="board._id" @click="setBoard(board._id)">{{board.title}}</li>
+      <li v-for="board in boards" :key="board._id" @click="setBoard(board._id)">{{ board.title }}</li>
       <li @click="createBoard">Add board</li>
     </ul>
   </section>
 </template>
 
 <script>
-
 export default {
-  name: "Home",
+  name: 'Home',
   data() {
     return {
-      user: null
+      user: null,
     };
   },
   async created() {
     // get userId
-    await this.$store.dispatch({ type: "loadBoards" });
+    await this.$store.dispatch({ type: 'loadBoards' });
   },
   computed: {
     boards() {
       return this.$store.getters.boards;
-    }
+    },
   },
   methods: {
     async createBoard() {
-      const title = prompt("add title");
-       if(!title) return
+      const title = prompt('add title');
+      if (!title) return;
 
       try {
         const board = await this.$store.dispatch({
-          type: "createBoard",
-          title
+          type: 'createBoard',
+          title,
         });
-        this.$router.push("/b/" + board._id);
+        this.$router.push('/b/' + board._id);
       } catch (err) {
-        console.log("cant get new board", err);
+        console.log('cant get new board', err);
       }
     },
     async setBoard(boardId) {
       try {
-        await this.$store.dispatch({ type: "loadAndWatchBoard", boardId });
-        this.$router.push("/b/" + boardId);
+        await this.$store.dispatch({ type: 'loadAndWatchBoard', boardId });
+        this.$router.push('/b/' + boardId);
       } catch (err) {
-        console.log("cant get board" + boardId, err);
+        console.log('cant get board' + boardId, err);
       }
-    }
+    },
   },
-  components: {}
+  components: {},
 };
 </script>

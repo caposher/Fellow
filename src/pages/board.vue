d49e07a34ee05ee753eb8b19a0002dd33ee31061
adamberco <adamberco@gmail.com>
2021-12-03 00:28:31 +0200

    added drag and scroll

--- src/pages/board.vue
+++ src/pages/board.vue
@@ -1,7 +1,11 @@
 <template>
-  <section v-if="board" :class="{ 'display-modal': selectedCardId }" class="board-app">
+  <section
+    v-if="board"
+    :class="{ 'display-modal': selectedCardId }"
+    class="board-app"
+  >
     <board-header />
-    <ul class="board">
+    <ul class="board" @click="scroll">
       <li class="list-wrapper" v-for="list in board.lists" :key="list.id">
         <board-list :list="list" @update="updateList"></board-list>
       </li>
@@ -16,10 +20,10 @@
 </template>
 
 <script>
-import boardHeader from '../cmps/board-header.cmp.vue';
-import mainMenu from '../cmps/main-menu.cmp.vue';
-import boardMenu from '../cmps/board-menu.cmp.vue';
-import boardList from '../cmps/board-list.cmp.vue';
+import boardHeader from "../cmps/board-header.cmp.vue";
+import mainMenu from "../cmps/main-menu.cmp.vue";
+import boardMenu from "../cmps/board-menu.cmp.vue";
+import boardList from "../cmps/board-list.cmp.vue";
 
 export default {
   data() {
@@ -28,7 +32,7 @@ export default {
     };
   },
   watch: {
-    '$route.params.cardId': {
+    "$route.params.cardId": {
       async handler() {
         // console.log(this.$route.params);
         const { cardId } = this.$route.params;
@@ -36,24 +40,24 @@ export default {
         if (cardId) {
           try {
             await this.$store.dispatch({
-              type: 'setListAndCard',
+              type: "setListAndCard",
               boardId,
               cardId,
             });
             this.selectedCardId = cardId;
           } catch (err) {
-            console.log('problem with getting board', err);
+            console.log("problem with getting board", err);
           }
         } else {
           try {
             await this.$store.dispatch({
-              type: 'setListAndCard',
-              boardId: '',
-              cardId: '',
+              type: "setListAndCard",
+              boardId: "",
+              cardId: "",
             });
             this.selectedCardId = null;
           } catch (err) {
-            console.log('problem with getting board', err);
+            console.log("problem with getting board", err);
           }
         }
       },
@@ -61,12 +65,13 @@ export default {
     },
   },
   async created() {
+    // this.scroll();
     const { boardId } = this.$route.params;
     console.log();
     try {
-      await this.$store.dispatch({ type: 'loadBoard', boardId });
+      await this.$store.dispatch({ type: "loadBoard", boardId });
     } catch (err) {
-      console.log('problem with getting board', err);
+      console.log("problem with getting board", err);
     }
   },
   computed: {
@@ -76,25 +81,58 @@ export default {
   },
   methods: {
     async addList() {
-      const title = prompt('list title');
+      const title = prompt("list title");
       if (!title) return;
       try {
         await this.$store.dispatch({
-          type: 'addList',
+          type: "addList",
           title,
           boardId: this.board._id,
         });
       } catch (err) {
-        console.log('cant add list', err);
+        console.log("cant add list", err);
       }
     },
 
     async updateList(list) {
       try {
-        await this.$store.dispatch({ type: 'updateList', list, boardId: this.board._id });
+        await this.$store.dispatch({
+          type: "updateList",
+          list,
+          boardId: this.board._id,
+        });
       } catch (err) {
-        console.log('cant update list', err);
+        console.log("cant update list", err);
       }
+    },
+
+    scroll(ev) {
+      const slider = ev.target;
+      let isDown = false;
+      let startX;
+      let scrollLeft;
+
+      slider.addEventListener("mousedown", (e) => {
+        isDown = true;
+        slider.classList.add("active");
+        startX = e.pageX - slider.offsetLeft;
+        scrollLeft = slider.scrollLeft;
+      });
+      slider.addEventListener("mouseleave", () => {
+        isDown = false;
+        slider.classList.remove("active");
+      });
+      slider.addEventListener("mouseup", () => {
+        isDown = false;
+        slider.classList.remove("active");
+      });
+      slider.addEventListener("mousemove", (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - slider.offsetLeft;
+        const walk = (x - startX) * 2; //scroll-fast
+        slider.scrollLeft = scrollLeft - walk;
+      });
     },
   },
   components: {

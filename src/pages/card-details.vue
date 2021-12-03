ab83e7eb147fd46e3c1794600a9083bcb5264c6a
adamberco <adamberco@gmail.com>
2021-12-03 21:23:38 +0200

    commit

--- src/pages/card-details.vue
+++ src/pages/card-details.vue
@@ -1,5 +1,9 @@
 <template>
-  <div v-if="list && cardToEdit" class="card-details-container" @click.stop.prevent="closeModal">
+  <div
+    v-if="list && cardToEdit"
+    class="card-details-container"
+    @click.stop.prevent="closeModal"
+  >
     <div class="card-details" @click.stop>
       <button @click.stop="closeModal" class="close">
         <span class="icon-md icon-close"></span>
@@ -30,18 +34,29 @@
                     }"
                   >
                     <!-- <span class="checkbox"> -->
-                    <input type="checkbox" v-model="cardToEdit.isComplete" @change="updateCard" />
+                    <input
+                      type="checkbox"
+                      v-model="cardToEdit.isComplete"
+                      @change="updateCard"
+                    />
                     <!-- </span> -->
                   </span>
                   <div class="date-picker">
                     <span>{{ dateToShow }}</span>
                     <span
-                      v-show="!cardToEdit.isComplete && +new Date(cardToEdit.dueDate) - Date.now() <= 86400000"
+                      v-show="
+                        !cardToEdit.isComplete &&
+                        +new Date(cardToEdit.dueDate) - Date.now() <= 86400000
+                      "
                       :class="timeLabelColor"
                       class="time-label"
                       >{{ timeLabel }}</span
                     >
-                    <span v-show="cardToEdit.isComplete" class="time-label complete">complete</span>
+                    <span
+                      v-show="cardToEdit.isComplete"
+                      class="time-label complete"
+                      >complete</span
+                    >
                   </div>
                 </div>
               </div>
@@ -68,7 +83,11 @@
               <!-- <span class="fa fa-align-left"></span> -->
               <div class="content">
                 <h3>Description</h3>
-                <button v-show="cardToEdit.description && !isEditDesc" @click.stop="setFocus" class="action-btn">
+                <button
+                  v-show="cardToEdit.description && !isEditDesc"
+                  @click.stop="setFocus"
+                  class="action-btn"
+                >
                   Edit
                 </button>
               </div>
@@ -88,7 +107,11 @@
             </div>
           </div>
 
-          <div class="check-list" v-for="checklist in cardToEdit.checklists" :key="checklist.id">
+          <div
+            class="check-list"
+            v-for="checklist in cardToEdit.checklists"
+            :key="checklist.id"
+          >
             <span class="card-details-icon icon-lg"></span>
 
             <checklist :checklist="checklist" @updateCL="updateCL" />
@@ -108,13 +131,9 @@
           </button>
           <h3>Add to card</h3>
           <!-- side menu renders cmp in click -->
-<<<<<<< HEAD
           <button class="action-btn not-yet" title="Not yet developed">
             <span class="icon-sm icon-member"></span>Members
           </button>
-=======
-          <button class="action-btn"><span class="icon-sm icon-member"></span>Members</button>
->>>>>>> 3c18662e1a351c43e99e9cf52a58a2c49f0df057
           <button @click.stop="toggleLabels" class="action-btn">
             <span class="icon-sm icon-label"></span>
             <span>Labels</span>
@@ -144,7 +163,11 @@
               </section>
               <form @submit.prevent="addCheckList">
                 <label>Title</label>
-                <input type="text" value="Checklist" v-model="newChecklist.title" />
+                <input
+                  type="text"
+                  value="Checklist"
+                  v-model="newChecklist.title"
+                />
                 <label>Copy items from...</label>
                 <select name id class="not-yet">
                   <option value>(none)</option>
@@ -153,7 +176,6 @@
               </form>
             </section>
           </section>
-<<<<<<< HEAD
           <date
             @updateDate="updateDate"
             :cardDate="cardToEdit.dueDate"
@@ -190,11 +212,6 @@
               <span class="icon-sm icon-share"></span>Share
             </button>
           </section>
-=======
-          <date @updateDate="updateDate" :cardDate="cardToEdit.dueDate" class="date"></date>
-          <button class="attachment action-btn"><span class="icon-sm icon-attach"></span>Attachments</button>
-          <button class="cover action-btn"><span class="icon-sm icon-cover"></span>Cover</button>
->>>>>>> 3c18662e1a351c43e99e9cf52a58a2c49f0df057
         </div>
       </div>
     </div>
@@ -202,11 +219,11 @@
 </template>
 
 <script>
-import cardLabels from '../cmps/labels.cmp.vue';
-import date from '../cmps/date.cmp.vue';
+import cardLabels from "../cmps/labels.cmp.vue";
+import date from "../cmps/date.cmp.vue";
 
-import { utilService } from '../services/util.service.js';
-import checklist from '../cmps/checklist.cmp.vue';
+import { utilService } from "../services/util.service.js";
+import checklist from "../cmps/checklist.cmp.vue";
 export default {
   data() {
     return {
@@ -215,7 +232,7 @@ export default {
       showDate: false,
       showLabels: false,
       openCheckList: false,
-      newChecklist: { title: 'Checklist' },
+      newChecklist: { title: "Checklist" },
       cardToEdit: null,
       isUndoDesc: false,
     };
@@ -237,23 +254,30 @@ export default {
       return this.$store.getters.boardId;
     },
     timeLabelColor() {
-      return +new Date(this.cardToEdit.dueDate) - Date.now() <= 0 ? 'over-due' : 'due-soon';
+      return +new Date(this.cardToEdit.dueDate) - Date.now() <= 0
+        ? "over-due"
+        : "due-soon";
     },
     timeLabel() {
-      return +new Date(this.cardToEdit.dueDate) - Date.now() <= 0 ? 'over due' : 'due soon';
+      return +new Date(this.cardToEdit.dueDate) - Date.now() <= 0
+        ? "over due"
+        : "due soon";
     },
     dateToShow() {
       const dateString = this.cardToEdit.dueDate;
       const dueDate = new Date(dateString);
       const time = this.formatAMPM(dueDate);
-      if (new Date().getDate() === new Date(dateString).getDate()) return 'today' + time;
-      else if (new Date().getDate() + 1 === new Date(dateString).getDate()) return 'tomorrow' + time;
-      else if (new Date().getDate() - 1 === new Date(dateString).getDate()) return 'yesterday' + time;
+      if (new Date().getDate() === new Date(dateString).getDate())
+        return "today" + time;
+      else if (new Date().getDate() + 1 === new Date(dateString).getDate())
+        return "tomorrow" + time;
+      else if (new Date().getDate() - 1 === new Date(dateString).getDate())
+        return "yesterday" + time;
       else if (new Date().getYear() === new Date(dateString).getYear()) {
         return this.formatDate(dueDate) + time;
       }
 
-      return this.formatDate(dueDate) + ', ' + dueDate.getFullYear() + time;
+      return this.formatDate(dueDate) + ", " + dueDate.getFullYear() + time;
     },
     getLabels() {
       const allLabels = this.$store.getters.labels;
@@ -265,11 +289,11 @@ export default {
     formatAMPM(dueDate) {
       var hours = dueDate.getHours();
       var minutes = dueDate.getMinutes();
-      var ampm = hours >= 12 ? 'PM' : 'AM';
+      var ampm = hours >= 12 ? "PM" : "AM";
       hours = hours % 12;
       hours = hours ? hours : 12;
-      minutes = minutes < 10 ? '0' + minutes : minutes;
-      var strTime = ' at ' + hours + ':' + minutes + ' ' + ampm;
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      var strTime = " at " + hours + ":" + minutes + " " + ampm;
       return strTime;
     },
     formatDate(dueDate) {
@@ -280,14 +304,14 @@ export default {
     },
     closeModal() {
       const { boardId } = this.$route.params;
-      this.$router.push('/b/' + boardId);
+      this.$router.push("/b/" + boardId);
     },
     async updateCard() {
       this.isEditDesc = false;
       // console.log(this.$route.matched[0].path);
       try {
         await this.$store.dispatch({
-          type: 'updateCard',
+          type: "updateCard",
           boardId: this.boardId,
           list: JSON.parse(JSON.stringify(this.list)),
           card: JSON.parse(JSON.stringify(this.cardToEdit)),
@@ -296,7 +320,7 @@ export default {
         // this.$router.matched[0].path.reload()
         // console.log("card updated with new desc");
       } catch (err) {
-        console.log('cant update card', err);
+        console.log("cant update card", err);
       }
     },
     async removeCard() {
@@ -341,24 +365,26 @@ export default {
 
     async addCheckList(ev) {
       if (!ev.target[0].value) return;
-      this.newChecklist.id = 'CL' + utilService.makeId();
+      this.newChecklist.id = "CL" + utilService.makeId();
       this.cardToEdit.checklists.push(this.newChecklist);
       this.openCheckList = false;
       try {
         await this.updateCard();
         this.newChecklist = {};
       } catch (err) {
-        console.log('cant update card', err);
+        console.log("cant update card", err);
       }
     },
     async updateCL(checklist) {
-      const idx = this.cardToEdit.checklists.findIndex((cl) => cl.id === checklist.id);
+      const idx = this.cardToEdit.checklists.findIndex(
+        (cl) => cl.id === checklist.id
+      );
       if (checklist.title) this.cardToEdit.checklists.splice(idx, 1, checklist);
       else this.cardToEdit.checklists.splice(idx, 1);
       try {
         await this.updateCard();
       } catch (err) {
-        console.log('cant save the todo', err);
+        console.log("cant save the todo", err);
       }
     },
     async updateLabels(labelIds) {

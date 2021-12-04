d8201769f457de6194d6fae347470e5df0a42b9c
adamberco <adamberco@gmail.com>
2021-12-04 18:10:43 +0200

    fixed adding card

--- src/services/board.service.js
+++ src/services/board.service.js
@@ -163,7 +163,7 @@ function getEmptyCard(title) {
       imgUrl: 'http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg',
     },
     activities: [],
-    imgUrl,
+    imgUrl: ''
   };
 }
 

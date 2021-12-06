893b576ef073c3afc874bb1259437efdf90a17b2
Oshra Hartuv <oshrartuv@gmail.com>
2021-12-06 13:47:45 +0200

    sone cover changes

--- src/cmps/cover.cmp.vue
+++ src/cmps/cover.cmp.vue
@@ -38,8 +38,12 @@ export default {
       colors: ["#7bc86c", "#f5dd29", "#ffaf3f", "#ef7564", "#cd8de5", "#5ba4cf", "#29cce5", "#6deca9", "#ff8ed4", "#172b4d"]
     };
   },
-  created(){
-
+  async created(){
+try{
+await this.$store.dispatch({type: 'requestPhotos',searchKey: 'view',  imgNum:6 })
+}catch(err){
+  console.log('cant get unsplash imgs', err);
+}
   },
   methods: {
     makeCover(val) {
